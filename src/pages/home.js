ee0f77cc78fa7a5e8685270fb15e19bd9eeb89fc
Utkarsh <20bec116@iiitdmj.ac.in>
2021-04-19 22:29:15 +0530

    beta commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,17 +5,49 @@ import Featured from "../Components/featured";
 import Reviews from "../Components/reviews";
 import Newsletter from "../Components/newsletter";
 import Footer from "../Components/footer";
+import styled from "styled-components";
 const Home = () => {
   return (
-    <>
-      <NavBar />
-      <Intro />
-      <Featured />
-      <Reviews />
-      <Newsletter />
-      <Footer />
-    </>
+    <WrapperHome>
+      <h2 className="extra">Please Use Desktop for better Experience</h2>
+      <h3 className="extra1">Thank You</h3>
+      <div className="extraBlock">
+        <NavBar />
+        <Intro />
+        <Featured />
+        <Reviews />
+        <Newsletter />
+        <Footer />
+      </div>
+    </WrapperHome>
   );
 };
-
+const WrapperHome = styled.section`
+  @media screen and (max-width: 1480px) {
+    .extraBlock {
+      display: none;
+    }
+    .extra {
+      font-size: 30px;
+      text-align: center;
+      margin-top: 10rem;
+    }
+    .extra1 {
+      font-size: 24px;
+      text-align: center;
+      margin-top: 1rem;
+    }
+  }
+  @media screen and (min-width: 1480px) {
+    .extraBlock {
+      display: all;
+    }
+    .extra {
+      display: none;
+    }
+    .extra1 {
+      display: none;
+    }
+  }
+`;
 export default Home;
